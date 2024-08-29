a6252836fe65c22d9c811d2eed0d7ca044dc6970
dawidzabrzenski <92026489+dawidzabrzenski@users.noreply.github.com>
2024-08-29 07:55:07 +0200

    cart v1

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -8,7 +8,7 @@ function AppLayout() {
   const isLoading = navigation.state === "loading";
 
   return (
-    <div className="grid h-screen grid-rows-[auto_1fr_auto] gap-4">
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
 
       <Header />
